1cf8d485431ddb0c52026a284414cf5f3c3016f2
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-12-09 10:33:31 +0530

    Update mysql_login.sh

--- mysql_login.sh
+++ mysql_login.sh
@@ -1,4 +1,4 @@
-  python3 ./secret_value.py > load.json
+  python3 secret_value.py > load.json
   chmod 711 ./load.json
   DB_USER=$(awk -F '"' '{print $4}' load.json)
   DB_PASS=$(awk -F '"' '{print $8}' load.json)
