b2bcb2012138bd5aa5a52129b747d0a1194699af
Ubuntu <ubuntu@ip-172-31-42-200.ap-south-1.compute.internal>
2022-12-09 04:30:03 +0000

    changes

--- mysql_login.sh
+++ mysql_login.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-  DB_USER='venkat'
-  DB_PASS='Root@123'
-  DB='Employee'
+  python3 ./secret_value.py > load.json
+  chmod 711 ./load.json
+  DB_USER=$(awk -F '"' '{print $4}' load.json)
+  DB_PASS=$(awk -F '"' '{print $8}' load.json)
   echo 'logging into db $DB as $DB_USER'
   mysql -u "$DB_USER" --password="$DB_PASS" < data.sql
