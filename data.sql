ab4d01df1a4755dd161991404fb76c7e0bd1f6d2
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-12-09 09:01:21 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,2 +1,2 @@
 USE Employee1;
-Insert into Employee1.Employee( EmpId, FirstName, LastName, Email, PhoneNo, Salary) values ( 003, "Mohamad", "malik", "Malik@gmail.com", "92514254447", 10000);
+Insert into Employee1.Employee( EmpId, FirstName, LastName, Email, PhoneNo, Salary) values ( 003, "shoib", "malik", "shoib@gmail.com", "6352417898", 1000);
