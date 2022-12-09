a489cb6aa6e155b5a64a613c86011955bd79c87c
Ubuntu <ubuntu@ip-172-31-42-200.ap-south-1.compute.internal>
2022-12-09 05:15:47 +0000

    changes

--- mysql_login.sh
+++ mysql_login.sh
@@ -1,5 +1,5 @@
-  python3 secret_value.py > load.json
-  chmod 711 ./load.json
+  #python3 secret_value.py > load.json
+  #chmod 711 ./load.json
   DB_USER=$(awk -F '"' '{print $4}' load.json)
   DB_PASS=$(awk -F '"' '{print $8}' load.json)
   echo 'logging into db $DB as $DB_USER'
